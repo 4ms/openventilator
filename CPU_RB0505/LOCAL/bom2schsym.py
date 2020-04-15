ded0e25a1fac9c49c0cf32abc1a59ef6d2ded740
Dan Green <danngreen1@gmail.com>
2020-04-15 15:27:11 -0700

    script imports from csv into schematic symbols (WIP)

--- CPU_RB0505/LOCAL/bom2schsym.py
+++ CPU_RB0505/LOCAL/bom2schsym.py
@@ -9,122 +9,116 @@ import csv
 import re
 
 sch_files = [f for f in os.listdir('.') if f.endswith('.sch')]
-csvfile = open('bom.csv')
+bomcsv_filename = 'bom.csv'
 
-libparts = {}
-unique = ""
-dups = ""
-partlines=""
-is_unique = True
-
-reader = csv.DictReader(csvfile)
-
-def loadAllSchFiles():
-    dat = {}
+def loadAllSchDat():
+    schdat = {}
     for sch in sch_files:
         with open(sch) as f:
-            dat[sch] = f.read()
-    return dat
+            print("Reading file: "+sch)
+            schdat[sch] = f.read()
+    return schdat
 
+def writeSchDatToFiles(schdat):
+    for sch in schdat:
+        with open(sch, "w") as f:
+            print("Writing file: "+sch)
+            f.write(schdat[sch])
 
-def findLibByRefInSchs(ref):
-    regex = re.compile('L ([^ ]*) ' + ref)
-    for sch in sch_files:
-        fil = open(sch)
-        dat = fil.read()
-        fil.close()
-        libname = regex.findall(dat)
-        if len(libname) > 0:
+def findRefInSchdat(ref, schdat):
+    regex = re.compile(r'\nL ([^ ]*) ' + ref + r'\n')
+    matching_sch = None
+    for sch in schdat:
+        print("Searching in "+sch)
+        hit = regex.findall(schdat[sch])
+        if len(hit) > 0:
+            matching_sch = sch
+            #Todo: report an error if len(hit)>1
             break
-    if len(libname) > 0:
-        return libname[0]
+    return matching_sch
+
+def findXYPos(ref, filetext):
+    m = re.search(r'\nL .*? '+ref+r'\n.*?\nP ([\d]+) ([\d]+)\n', filetext, re.MULTILINE)
+    if m is not None:
+        return (m.group(1), m.group(2))
     else:
-        return ''
+        return None
+
+def addFieldToSymInSch(ref, lines_to_insert, filetext):
+    x,y = findXYPos(ref, filetext)
+    xy_lines = re.sub(r'(&X&) (&Y&)', x+' '+y, lines_to_insert)
+    newfile, numfound = re.subn(r'(\nL .*? '+ref+r'\n.*?)(\n\s+[01])', r'\1\n' + xy_lines + r'\2', filetext, re.MULTILINE, re.DOTALL)
+    return (newfile, numfound)
+
+def addFieldsToAllRefs(refslist, schlines, schdat):
+    for ref in refslist:
+        if len(ref)==0:
+            print("Ref is blank")
+            return
+        
+        found_sch = findRefInSchdat(ref, schdat)
+        if found_sch is None:
+            print("Ref not found in any .sch file")
+            return
+
+        newfiletext, numfound = addFieldToSymInSch(ref, schlines, schdat[found_sch])
+        if numfound>0:
+            print("Found "+ref+" in file "+found_sch)
+            schdat[found_sch] = newfiletext
+        else:
+            print("Not Found: "+ref)
+
+def mergeBomIntoSch():
+
+    schlines=""
+    sch_tags = "H &X& &Y& 50  0001 C CNN"
+    #lib_tags = "0 0 50 H I C CNN"
+    refslist=[]
+
+    schdat = loadAllSchDat()
+    csvfile = open(bomcsv_filename)
+    print("Using bom csv file: "+bomcsv_filename)
+    reader = csv.DictReader(csvfile)
+    i=0
+    for row in reader:
+        itemnum = row['Item Number']
+        refs = row['Ref Des']
+        desc = row['Item Description']
+        manu = row['Mfr. Name']
+        manuNo = row['Mfr. Part Number']
+        manuDesc = row['Mfr. Part Description']
+        # print("Parsing Row#"+str(i))
+        i=i+1
+        if len(itemnum) > 0:
+            if len(refslist)>0:
+                print("Adding fields to schematic:")
+                print(refslist)
+                print(schlines)
+                addFieldsToAllRefs(refslist, schlines, schdat)
+
+            print("Found new item number "+itemnum+" on row "+str(i))
+            schlines = ""
+            refslist = re.split(r',\s*', refs)
+            extras_idx = 2
+            f_num = 8
+
+            schlines = schlines + "F4 \""+desc+"\" "+ sch_tags + " \"Description\"" + "\n"
+            schlines = schlines + "F5 \""+itemnum+"\" "+ sch_tags + " \"Item Number\"" + "\n"
+            schlines = schlines + "F6 \""+manu+"\" "+ sch_tags + " \"Manufacturer\"" + "\n"
+            schlines = schlines + "F7 \""+manuNo+"\" "+ sch_tags + " \"Manufacturer_No\"" + "\n"
+            schlines = schlines + "F8 \""+manuDesc+"\" "+ sch_tags + " \"Manufacturer_Desc\"" + "\n"
+
+        else:
+            schlines = schlines + "F"+str(f_num+1)+" \""+manu+"\" "+ sch_tags + " \"Manufacturer"+str(extras_idx)+"\"" + "\n"
+            schlines = schlines + "F"+str(f_num+2)+" \""+manuNo+"\" "+ sch_tags + " \"Manufacturer_No"+str(extras_idx)+"\"" + "\n"
+            schlines = schlines + "F"+str(f_num+3)+" \""+manuDesc+"\" "+ sch_tags + " \"Manufacturer_Desc"+str(extras_idx)+"\"" + "\n"
+            extras_idx = extras_idx + 1
+            f_num = f_num + 3
+
+    return schdat
 
 
-def output_last_item_as_libsym(partlines):
-    if is_unique is True:
-        unique = unique + partlines
-    else:
-        dups = dups + partlines
+##main
+new_sch_filedat = mergeBomIntoSch()
+writeSchDatToFiles(new_sch_filedat)
 
-def output_last_item_as_schsym(partlines):
-    return
-
-for row in reader:
-    itemnum = row['Item Number']
-    refs = row['Ref Des']
-    desc = row['Item Description']
-    manu = row['Mfr. Name']
-    manuNo = row['Mfr. Part Number']
-    manuDesc = row['Mfr. Part Description']
-
-    if len(itemnum) > 0:
-        #New item number
-        output_last_item_as_libsym(partlines)
-
-        is_unique = True
-        partlines = "\n"
-        partlines = partlines + "RefDes: " + refs + "\n"
-        if len(refs) > 0:
-            if "," in refs:
-                commapos = refs.find(",")
-                ref = refs[:commapos]
-            else:
-                ref = refs
-
-            libname = findLibByRefInSchs(ref)
-            if len(libname)>0:
-                libpart = libname[libname.find(":")+1:]
-                partlines = partlines + "Lib Part: " + libpart + "\n"
-                if libpart in libparts.keys():
-                    is_unique = False
-                else:
-                    libparts[libpart] = True
-
-        extras_idx = 1
-        f_num = 8
-        desc_line = "F4 \""+desc+"\" 0 0 50 H I C CNN \"Description\""
-        desc_sym_line = "F4 \""+desc+"\" H 0   0   50  0001 C CNN \"Description\""
-        itemnum_line = "F5 \""+itemnum+"\" 0 0 50 H I C CNN \"Item Number\""
-        itemnum_sym_line = "F5 \""+itemnum+"\" 0 0 50 H I C CNN \"Item Number\""
-        manu_line = "F6 \""+manu+"\" 0 0 50 H I C CNN \"Manufacturer\""
-        manu_sym_line = "F6 \""+manu+"\" 0 0 50 H I C CNN \"Manufacturer\""
-        manuNo_line = "F7 \""+manuNo+"\" 0 0 50 H I C CNN \"Manufacturer_No\""
-        manuNo_sym_line = "F7 \""+manuNo+"\" 0 0 50 H I C CNN \"Manufacturer_No\""
-        manuDesc_line = "F8 \""+manuDesc+"\" 0 0 50 H I C CNN \"Manufacturer_Desc\""
-        manuDesc_sym_line = "F8 \""+manuDesc+"\" 0 0 50 H I C CNN \"Manufacturer_Desc\""
-        partlines = partlines + desc_line + "\n"
-        partlines = partlines + itemnum_line + "\n"
-        partlines = partlines + manu_line + "\n"
-        partlines = partlines + manuNo_line + "\n"
-        partlines = partlines + manuDesc_line + "\n"
-    else:
-        #more manuf for current item number
-        extras_idx = extras_idx + 1
-        f_num = f_num + 1
-        manu_extra_line = "F"+str(f_num)+" \""+manu+"\" 0 0 50 H I C CNN \"Manufacturer"+str(extras_idx)+"\""
-        f_num = f_num + 1
-        manuNo_extra_line = "F"+str(f_num)+" \""+manuNo+"\" 0 0 50 H I C CNN \"Manufacturer_No"+str(extras_idx)+"\""
-        f_num = f_num + 1
-        manuDesc_extra_line = "F"+str(f_num)+" \""+manuDesc+"\" 0 0 50 H I C CNN \"Manufacturer_Desc"+str(extras_idx)+"\""
-        partlines = partlines + manu_extra_line + "\n"
-        partlines = partlines + manuNo_extra_line + "\n"
-        partlines = partlines + manuDesc_extra_line + "\n"
-
-# symbol format:
-# F 4 "CAPACITOR CERAMIC, 100NF, +/-10%, 50V, X7R,0805" H 0   0   50  0001 C CNN "Description"
-# F 5 "10022135" H 0   0   50  0001 C CNN "Item Number"
-# F 6 "YAGEO_RMS1" H 0   0   50  0001 C CNN "Manufacturer"
-
-# Format (for reference)
-# F4 "TRANSISTOR MOSFET N CHANNEL 60V, 0.18A" 0 0 50 H I C CNN "Description"
-# F5 "10097274" 0 0 50 H I C CNN "Item Number"
-# F6 "ON SEMICONDUCTOR_RMS1" 0 0 50 H I C CNN "Manufacturer"
-# F7 "2N7002LT1G" 0 0 50 H I C CNN "Manufacturer_No"
-# etc..
-
-print("Unique Library Parts:")
-print(unique)
-print("Duplicates:")
-print(dups)
