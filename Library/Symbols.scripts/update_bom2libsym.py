d02f336b974889ac81cefb562b4c78d513fb5e61
Dan Green <danngreen1@gmail.com>
2020-05-12 09:48:28 -0700

    update script WIP

--- Library/Symbols.scripts/update_bom2libsym.py
+++ Library/Symbols.scripts/update_bom2libsym.py
@@ -20,6 +20,8 @@ import csv
 import re
 from pathlib import Path
 
+notfound = []
+
 def loadFileList(file_name_list):
     dat = {}
     for fil in file_name_list:
@@ -32,7 +34,7 @@ def loadLibraryFiles(symbol_dir):
     lib_filenames = [symbol_dir+f for f in os.listdir(symbol_dir) if f.endswith('.lib')]
     libdat = loadFileList(lib_filenames)
     return libdat
-       
+
 def extractFieldsFromBoms(bom_dir):
     field_dict={}
     bom_filenames = [bom_dir+f for f in os.listdir(bom_dir) if f.endswith('.csv')]
@@ -74,7 +76,7 @@ def extractFieldsFromBoms(bom_dir):
                             field_dict[last_itemnum]['fields'] = field_list
                             #print("Extracted fields for Item Number: "+last_itemnum + " ("+bom_filename+")")
 
-                        if found_duplicate_itemnum: 
+                        if found_duplicate_itemnum:
                             print("**Item Number "+last_itemnum+" found in file: "+bom_filename+" and in file: "+other_filename)
                             diffs=""
                             other_flds = {}
@@ -132,28 +134,34 @@ def updateSymbolLibrary(field_dict, libs_dict):
     for itemnum, d in field_dict.items():
         fields = d['fields']
         for field_name, field_val, field_idx in fields:
-            updateOrInsertField(itemnum, field_name, field_val, field_idx, libs_dict)
+            isfound = updateOrInsertField(itemnum, field_name, field_val, field_idx, libs_dict)
+            if not isfound:
+                break
+    return libs_dict
 
 def updateOrInsertField(itemnum, field_name, field_val, field_idx, libs_dict):
+    global notfound
     lib, symbol_name = findSymbolByItemnum(itemnum, libs_dict)
     if lib is None:
         print("****ERROR****: Item Number: "+itemnum+" not found in any libraries")
-        return
+        if itemnum not in notfound:
+            notfound.append(itemnum)
+        return False
     if checkSymbolNeedsUpdating(symbol_name, field_name, field_val, libs_dict[lib]):
         libs_dict[lib], num = updateSymbolField(symbol_name, field_name, field_val, libs_dict[lib])
         if num > 1:
             print("ERROR: Symbol "+symbol_name+" found more than once in a single library file " + lib)
         if num > 0:
-            print("Updated symbol "+symbol_name+" in library "+lib+" with field "+field_name+" = "+field_val)
-        else:
+            print("Updated field "+field_name+" = "+field_val+ " into symbol "+symbol_name+" in library "+lib+".")
+        elif field_val!="" and field_name!="" and field_idx>=4:
             libs_dict[lib], num = insertSymbolField(symbol_name, field_name, field_val, field_idx, libs_dict[lib])
             if num>0:
                 print("Inserted field "+field_name+" = "+field_val+ " into symbol "+symbol_name+" in library "+lib+".")
             else:
                 print("ERROR: could not insert field "+field_name+" = "+field_val+ " into symbol "+symbol_name+" in library "+lib+".")
-    else:
-        print("No update necessary")
-    return libs_dict
+    # else:
+    #     print("No update necessary")
+    return True
 
 def findSymbolByItemnum(itemnum, libs_dict):
     """ Given an Item Number and library data dict
@@ -174,7 +182,7 @@ def checkSymbolNeedsUpdating(symbol_name, field_name, field_val, libfiledata):
     e_name = re.escape(field_name)
     e_val = re.escape(field_val)
     m = re.findall(
-            r'^DEF '+e_symbol+r' .*(?:\n(?!ENDDEF).*)+\nF ?\d+ "'+e_val+r'" .* "'+e_name+r'"\n', 
+            r'^DEF '+e_symbol+r' .*(?:\n(?!ENDDEF).*)+\nF ?\d+ "'+e_val+r'" .* "'+e_name+r'"\n',
             libfiledata, flags=re.MULTILINE)
     if len(m) == 0:
         return True
@@ -182,15 +190,15 @@ def checkSymbolNeedsUpdating(symbol_name, field_name, field_val, libfiledata):
         return False
 
 def updateSymbolField(symbol_name, field_name, field_val, libfiledata):
-    """ Substitute the given field value for a given symbol 
+    """ Substitute the given field value for a given symbol
         with the given name in libfiledata
     """
     e_symbol = re.escape(symbol_name)
     e_name = re.escape(field_name)
     e_val = re.escape(field_val)
     libfiledata, num = re.subn(
-            r'^(DEF '+e_symbol+r' .*(?:\n(?!ENDDEF).*)+\nF ?\d+ ")[^"]*(" .* "'+e_name+r'"\n)', 
-            r'\1'+e_val+r'\2', 
+            r'^(DEF '+e_symbol+r' .*(?:\n(?!ENDDEF).*)+\nF ?\d+ ")[^"]*(" .* "'+e_name+r'"\n)',
+            r'\g<1>'+e_val+r'\g<2>',
             libfiledata, flags=re.MULTILINE)
     return libfiledata, num
 
@@ -201,7 +209,7 @@ def insertSymbolField(symbol_name, field_name, field_val, field_idx, libfiledata
     field_txt = createFieldText(field_name, field_val, field_idx)
     libfiledata, num = re.subn(
             r'^(DEF '+e_symbol+r' .*(?:\nF.*)+\n)',
-            r'\1'+field_txt, 
+            r'\1'+field_txt,
             libfiledata, flags=re.MULTILINE)
     return libfiledata, num
 
@@ -228,15 +236,20 @@ def appendSlash(pathname):
 
 if __name__ == "__main__":
     if len(sys.argv) > 2:
-        bom_dir = appendSlash(sys.argv[1])
-        print("Using BOM directory: "+bom_dir)
-        lib_dir = appendSlash(sys.argv[2])
-        print("Will update symbol library files in: "+lib_dir)
+        boms_dir = appendSlash(sys.argv[1])
+        print("Using BOM directory: "+boms_dir)
+        libs_dir = appendSlash(sys.argv[2])
+        print("Will update symbol library files in: "+libs_dir)
 
-        flds = extractFieldsFromBoms(bom_dir)
-        lib_dict = loadLibraryFiles(lib_dir)
-        updateSymbolLibrary(flds, lib_dict)
-        writeLibraryFiles(lib_dict)
+        flds = extractFieldsFromBoms(boms_dir)
+        libs_dict = loadLibraryFiles(libs_dir)
+        libs_dict = updateSymbolLibrary(flds, libs_dict)
+        writeLibraryFiles(libs_dict)
+
+        if len(notfound)>0:
+            print("")
+            print("The following item numbers were not found in any libraries. You must correct this, or manually create new library symbols:")
+            print(', '.join(notfound))
 
         #print("Exporting CSV file:"+csv_output_filename)
         #exportSymbolListCSV(syms, csv_output_filename)
@@ -246,13 +259,20 @@ Please specify a dir with BOM csv files, and a symbol library dir.
 
 **********************************************************************
 Usage:
-$ python3 update_bom2libsym.py myBOMs/ ../mySymbols/
+$ python3 update_bom2libsym.py csvBOMs/ ../SymbolLibraryDir/
 **********************************************************************
 
-This script will scan all csv files in a directory. For each line with an Item
-Number, it will search for that Item Number in all .lib files in the Library
-dir (set by lib_dir).  Then it will update the Description, Datasheet, and Mfg
-Part number fields in the symbol library dir
+This script will scan all csv files in the given directory. For each group of
+lines beginning with an Item Number, it will search for that Item Number in all
+.lib files in the SymbolLibraryDir. Then it will update or insert the
+Description, Datasheet, and Manufacturer*, Manufacturer_No*, fields in the lib
+file.
+
+After running this script, to apply changes to schematics, you must run "Update
+Fields from Library" in Kicad schematic editor. It's recommended to check all
+fields except uncheck Reference, Value, and Footprint and any Manufacturer#
+fields with # higher than 2.  Also consider checking "Reset fields which are
+empty in library".
 
 """)
 
